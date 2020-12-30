64c0b91a7f46edeaf57cceadb27d964cf1c8bb66
NSDCode <47573987+NSDCode@users.noreply.github.com>
2020-12-30 02:49:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNGuard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModuleDefMD module = ModuleDefMD.Load(args[0]);
+            foreach (TypeDef type in module.Types)
+            {
+                if (!type.HasMethods)
+                    continue;
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (method.HasBody && method.Body.HasInstructions)
+                    {
+                        IList<Instruction> IL = method.Body.Instructions;
+                        for (int i = 0; i < IL.Count; i++)
+                        {
+                           if (IL[i].OpCode == OpCodes.Call && IL[i].Operand == null)
+                            {
+                                Console.WriteLine($"Detected Anti-Tamper @ {method.Name} ({i})");
+                                IL[i].OpCode = OpCodes.Nop;
+                                IL[i + 3].OpCode = OpCodes.Nop;
+                            }
+                        }
+                    }
+                }
+
+
+
+            }
+            ModuleWriterOptions ee = new ModuleWriterOptions(module);
+            ee.MetadataLogger = DummyLogger.NoThrowInstance;
+            module.Write($@"{Environment.CurrentDirectory}\Decoded.exe", ee);
+        }
+    }
+}
